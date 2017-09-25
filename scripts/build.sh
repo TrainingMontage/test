f8f4a926faee1c02ec0c9f475630f45746064c25
Alec Rosenbaum <alecr95@gmail.com>
2017-09-25 13:55:00 -0400

    working this time?

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,13 +2,8 @@
 # usage: build.sh <src_dir> <build_dir>
 for f in $(find $1 -name '*.tex'); do
     OUT_DIR=$2/$(dirname "${f#./}")
-    tree
-    echo "mkdir -p $OUT_DIR"
     mkdir -p $OUT_DIR
-    echo "latexmk -output-directory=$OUT_DIR -pdf $f"
     latexmk -output-directory=$OUT_DIR -pdf $f
-    echo "latexmk -c -output-directory=$OUT_DIR $f"
     latexmk -c -output-directory=$OUT_DIR $f
 done
-echo "gen_filetree"
-./scripts/gen_filetree.py $2
+python scripts/gen_filetree.py $2
