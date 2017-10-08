fb0f971372ba5e097d3ee55d1095d1a8756b9701
Isaac <isaacgoss14750@gmail.com>
2017-10-08 13:21:45 -0400

    basic sketch

---
+++ src/main/java/trackmodel/TrackModelInterface.java
@@ -0,0 +1,12 @@
+package trackmodel;
+
+public interface TrackModelInterface {
+    public static int getTrainAuthority(int trainId);
+    public static double getTrainSpeed(int trainId);
+    public static boolean setSignal(int blockId, boolean value);
+    public static boolean getSignal(int blockId);
+    public static boolean setSwitch(int blockId, boolean value);
+    public static boolean getSwitch(int blockId);
+    public static Authority setAuthority(int blockId, Authority auth);
+    public static int setSpeed(int blockId, int speed);
+}
