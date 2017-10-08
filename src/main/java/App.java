01e64bb4786a00985101f0bad6f5ae4beb0d7dd8
Isaac <isaacgoss14750@gmail.com>
2017-10-08 10:09:49 -0400

    App.java should run my app now

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -11,6 +11,9 @@
  *      tell us if they fail, build the docs, and push them to a GitHub Pages site.
  * </ol>
  */
+
+import wayside.WaysideControllerGUI;
+
 public class App {
     /**
      * A basic function that can be tested with simple code.
@@ -26,7 +29,32 @@ public class App {
      *
      * @param args Command-line arguments.
      */
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(WaysideControllerGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(WaysideControllerGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(WaysideControllerGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(WaysideControllerGUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new WaysideControllerGUI().setVisible(true);
+            }
+        });
     }
 }
