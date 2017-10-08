233684f5d8d66a16f522907642984d97cce466ae
Isaac <isaacgoss14750@gmail.com>
2017-10-08 12:45:00 -0400

    removed a method

--- src/main/java/trainmodel/TrainModelInterface.java
+++ src/main/java/trainmodel/TrainModelInterface.java
@@ -3,7 +3,6 @@ package trainmodel;
 public interface TrainModelInterface {
     public static int createTrain(int startingBlockId);
     public double getVelocity();
-    public void setPower(double power);
     public double getSuggestedSpeed();
     public double getAuthority();
     public double getMaxPower();
--- src/main/java/wayside/WCInterface.java
+++ src/main/java/wayside/WCInterface.java
@@ -3,7 +3,11 @@ package wayside;
 public interface WCInterface {
     public Block getAuthority(Block trainPosition);
     public int getSpeed(Block trainPosition);
-    public void suggest(Block trainPosition, Block authority, int speed);
+    public static void suggest(Block trainPosition, Block authority, int speed);
+    public static int[] getOccupiedBlocks();
+    public static int[] getBrokenBlocks();
+    public static int[] getGreenLights();
+
     public boolean occupiedBlock(Block block);
     public boolean lightState(Block lightPosition);
     public boolean switchState(Block switchPosition);
