fcd1c967257aaa31cf7971d150878718a133daad
Isaac <isaacgoss14750@gmail.com>
2017-10-08 12:52:46 -0400

    another small change

--- src/main/java/trainmodel/TrainModelInterface.java
+++ src/main/java/trainmodel/TrainModelInterface.java
@@ -3,6 +3,7 @@ package trainmodel;
 public interface TrainModelInterface {
     public static int createTrain(int startingBlockId);
     public double getVelocity();
+    public double getDisplacement();
     public double getSuggestedSpeed();
     public double getAuthority();
     public double getMaxPower();
