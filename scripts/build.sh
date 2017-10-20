833e4623d60647c69b7d229f4feb488a1d6a3113
Alec Rosenbaum <alecr95@gmail.com>
2017-10-20 15:15:14 -0400

    add basic metapost template

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,7 +3,7 @@
 for f in $(find $1 -name '*.tex'); do
     OUT_DIR=$2/$(dirname "${f#./}")
     mkdir -p $OUT_DIR
-    latexmk -quiet -output-directory=$OUT_DIR -pdf $f
+    latexmk -quiet -output-directory=$OUT_DIR -pdf $f --shell-escape
     latexmk -quiet -c -output-directory=$OUT_DIR $f
 done
 python scripts/gen_filetree.py $2
