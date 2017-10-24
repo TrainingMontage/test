9adc165280a830b76870bf80ad49640642112509
Isaac <isaacgoss14750@gmail.com>
2017-10-24 15:53:02 -0400

    IDK why I even made that change in the first place

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -29,7 +29,7 @@ public class App {
      *
      * @param args Command-line arguments.
      */
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         WaysideController.init();
     }
 }
