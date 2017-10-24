0e03379a2d078a39dd47394c845fab5fb374a209
Isaac <isaacgoss14750@gmail.com>
2017-10-24 15:52:08 -0400

    moved helpers to shared package

--- src/main/java/shared/Suggestion.java
+++ src/main/java/shared/Suggestion.java
@@ -1,5 +1,14 @@
 package shared;
 
+import java.util.List;
+
 public class Suggestion {
-    // TODO
+    public final int blockId;
+    public final int speed;
+    public final int[] authority;
+    public Suggestion(int b, int s, int[] a) {
+        blockId = b;
+        speed = s;
+        authority = a;
+    }
 }
--- src/main/java/utils/BlockStatus.java
+++
@@ -1,7 +0,0 @@
-package utils;
-
-public enum BlockStatus {
-    OPERATIONAL,
-    BROKEN,
-    IN_REPAIR
-}
