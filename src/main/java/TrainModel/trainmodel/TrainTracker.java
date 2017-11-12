585670c3ac86d1b79ed075a1961bfe262648fba4
Parth Dadhania <pad65@pitt.edu>
2017-11-12 14:04:39 -0500

    fixed compile error

--- src/main/java/TrainModel/trainmodel/TrainTracker.java
+++ src/main/java/TrainModel/trainmodel/TrainTracker.java
@@ -4,17 +4,20 @@ import java.text.DecimalFormat;
 import java.text.NumberFormat;
 import java.lang.Math;
 import java.*;
+import java.util.*;
 
 /**
  *
  * @author Parth
  */
 public class TrainTracker{
-    ArrayList trainList = new ArrayList();
+    ArrayList<Train> trainList = new ArrayList<Train>();
     int trainId = 0;
     int blockId = 0;
     public void createTrain(){
-        Train newTrain = new Train(trainId, blockId)
+        Train newTrain = new Train(trainId, blockId);
+        trainList.add(newTrain);
         trainId++;
+
     }
 }
