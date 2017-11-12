b6d4ad59b7e499cfc41b223c506b4940da2f57ca
Isaac <isaacgoss14750@gmail.com>
2017-11-12 15:34:41 -0500

    put launching windows into another thread

--- src/main/java/wayside/Runner.java
+++ src/main/java/wayside/Runner.java
@@ -3,5 +3,10 @@ package wayside;
 public class Runner {
     public static void main(String[] args) {
         WaysideController.init();
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                WaysideController.openWindow();
+            }
+        });
     }
 }
