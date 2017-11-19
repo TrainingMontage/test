2796ba925db4ab1c3b5d9fa698c7ff2318921517
Isaac <isaacgoss14750@gmail.com>
2017-11-19 17:35:06 -0500

    Added green line as option to PATHS

--- src/main/java/wayside/WaysideController.java
+++ src/main/java/wayside/WaysideController.java
@@ -53,19 +53,51 @@ public class WaysideController {
      * embedding information about the track in this module.
      * Also, this is only the Green line.
      */
-    static int TRACK_LEN = 151;
+    static int TRACK_LEN = 153;
     static int NUM_SWITCHES = 7;
     static int[] CROSSINGS = {19};
     static int[][] PATHS;
+    private static final int INTO_YARD = 151;
+    private static final int FROM_YARD = 152;
     
     static TrackModel tm = TrackModel.getTrackModel();
 
     /**
      * Initiallizes the WaysideController.
-     * For now, just working on one region.
+     * For now, just working on the green line,
+     * as though there's only one WC.
      */
     public static void init() {
-        // TODO
+        PATHS = new int[][] {
+            // The long circuit around the entire track.
+            new int[] {
+                // high-C downto low-A,
+                12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1,
+                // low-D upto high-Q,
+                13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28,
+                29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40, 41, 42, 43, 44,
+                45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60,
+                61, 62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76,
+                77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 91, 92,
+                93, 94, 95, 96, 97, 98, 99, 100,
+                // high-N downto low-N,
+                85, 84, 83, 82, 81, 80, 79, 78, 77,
+                // low-R upto high-Z
+                101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113,
+                114, 115, 116, 117, 118, 119, 120, 121, 122, 123, 124, 125, 126,
+                127, 128, 129, 130, 131, 132, 133, 134, 135, 136, 137, 138, 139,
+                140, 141, 142, 143, 144, 145, 146, 147, 148, 149, 150
+            },
+            // Leaving the yard, entering the track.
+            new int[] {
+                36, 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51,
+                52, 53, 54, 55, 56, 57, INTO_YARD
+            },
+            // Entering the yard from the track.
+            new int[] {
+                FROM_YARD, 63, 64, 65, 66, 67, 68
+            }
+        };
     }
 
     /**
