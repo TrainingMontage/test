3f451ad1f44743ea6b98989c5ac27d9ecd85f885
Isaac <isaacgoss14750@gmail.com>
2017-11-22 20:23:56 -0500

    my static block and switch compile!

---
+++ src/main/java/wayside/trackmock/WCStaticSwitch.java
@@ -0,0 +1,39 @@
+/*   ______                 _           _                 
+ *  /_  __/ _____  ____ _  (_) ____    (_) ____    ____ _ 
+ *   / /   / ___/ / __ `/ / / / __ \  / / / __ \  / __ `/ 
+ *  / /   / /    / /_/ / / / / / / / / / / / / / / /_/ /  
+ * /_/   /_/     \__,_/ /_/ /_/ /_/ /_/ /_/ /_/  \__, /   
+ *                                              /____/    
+ *     __  ___                 __                        
+ *    /  |/  / ____    ____   / /_  ____ _  ____ _  ___ 
+ *   / /|_/ / / __ \  / __ \ / __/ / __ `/ / __ `/ / _ \
+ *  / /  / / / /_/ / / / / // /_  / /_/ / / /_/ / /  __/
+ * /_/  /_/  \____/ /_/ /_/ \__/  \__,_/  \__, /  \___/ 
+ *                                       /____/         
+ *
+ * @author Isaac Goss
+ */
+
+package wayside.trackmock;
+
+import trackmodel.StaticBlock;
+import trackmodel.StaticSwitch;
+
+public class WCStaticSwitch extends StaticSwitch {
+    
+    public WCStaticSwitch(int id) {
+        super(id);
+    }
+
+    public void rootBlockSetter(StaticBlock sb) {
+        this.setRoot(sb);
+    }
+
+    public void activeLeafSetter(StaticBlock sb) {
+        this.setActiveLeaf(sb);
+    }
+
+    public void defaultLeafSetter(StaticBlock sb) {
+        this.setDefaultLeaf(sb);
+    }
+}
---
+++ src/main/java/wayside/trackmock/WCTrackModel.java
@@ -0,0 +1,122 @@
+/*   ______                 _           _                 
+ *  /_  __/ _____  ____ _  (_) ____    (_) ____    ____ _ 
+ *   / /   / ___/ / __ `/ / / / __ \  / / / __ \  / __ `/ 
+ *  / /   / /    / /_/ / / / / / / / / / / / / / / /_/ /  
+ * /_/   /_/     \__,_/ /_/ /_/ /_/ /_/ /_/ /_/  \__, /   
+ *                                              /____/    
+ *     __  ___                 __                        
+ *    /  |/  / ____    ____   / /_  ____ _  ____ _  ___ 
+ *   / /|_/ / / __ \  / __ \ / __/ / __ `/ / __ `/ / _ \
+ *  / /  / / / /_/ / / / / // /_  / /_/ / / /_/ / /  __/
+ * /_/  /_/  \____/ /_/ /_/ \__/  \__,_/  \__, /  \___/ 
+ *                                       /____/         
+ *
+ * @author Isaac Goss
+ */
+
+package wayside.trackmock;
+
+import shared.BlockStatus;
+import trackmodel.TrackModelInterface;
+import trackmodel.StaticBlock;
+import trackmodel.StaticSwitch;
+import static wayside.WaysideController.TRACK_LEN;
+
+/**
+ * A simple static track model aganist which to test WaysideController.
+ */
+public class WCTrackModel implements TrackModelInterface {
+    
+    private boolean[] authority;
+    private boolean[] occupied;
+    private int[] speed;
+
+    public WCTrackModel() {
+        int n = TRACK_LEN;
+        authority = new boolean[n];
+        occupied  = new boolean[n];
+        speed     = new int[n];
+        clear();
+    }
+
+    public void clear() {
+        for (int i = 0; i < speed.length; i++) {
+            authority[i] = false;
+            occupied[i] = false;
+            speed[i] = 0;
+        }
+    }
+
+    public boolean getTrainAuthority(int trainId) {
+        return authority[trainId];
+    }
+
+    public boolean isOccupied(int blockId) {
+        return occupied[blockId];
+    }
+
+    public BlockStatus getStatus(int blockId) {
+        return null;
+    }
+
+    public boolean setSignal(int blockId, boolean value) {
+        return false;
+    }
+
+    public boolean getSignal(int blockId) {
+        return false;
+    }
+
+    public boolean setSwitch(int blockId, boolean value) {
+        return false;
+    }
+
+    public boolean getSwitch(int blockId) {
+        return false;
+    }
+
+    public boolean setCrossingState(int blockId, boolean active) {
+        return false;
+    }
+
+    public boolean getCrossingState(int blockId) {
+        return false;
+    }
+
+    public boolean setAuthority(int blockId, boolean authority) {
+        return this.authority[blockId];
+    }
+
+    public int setSpeed(int blockId, int speed) {
+        return this.speed[blockId];
+    }
+
+    public StaticSwitch getStaticSwitch(int switchID) {
+        return null;
+    }
+
+    public StaticBlock getStaticBlock(int blockId) {
+        return null;
+    }
+
+    /* TRAIN METHODS I DON'T CARE ABOUT */
+    public double getTrainSpeed(int trainId) {
+        return 0;
+    }
+
+    public int getTrainBeacon(int trainId) {
+        return 0;
+    }
+
+    public int getTrainPassengers(int trainId) {
+        return 0;
+    }
+
+    public boolean isIcyTrack(int trainId) {
+        return false;
+    }
+
+    public double getGrade(int trainId) {
+        return 0;
+    }
+}
