5814ab909ac583ece944f4da76ea9d9fe47dcfde
Isaac <isaacgoss14750@gmail.com>
2017-11-30 15:00:44 -0500

    reading TRACK_LEN from plc file

--- src/main/java/wayside/PlcImporter.java
+++ src/main/java/wayside/PlcImporter.java
@@ -6,10 +6,18 @@ import java.util.Scanner;
 
 public class PlcImporter {
 
+    private int track_len;
+
     public PlcImporter(File file) throws IOException {
         Scanner in = new Scanner(file);
-        while (in.hasNextLine()) {
+        read_file: while (in.hasNextLine()) {
             String temp = in.nextLine();
+            String[] line = temp.split("=");
+            switch (line[0].trim()) {
+                case "END": break read_file;
+                case "TRACK_LEN":
+                    track_len = Integer.parseInt(line[1].trim());
+            }
         }
     }
 
