6cbc1a94f8140983d9a455cd9b068e69a156e60e
Alec Rosenbaum <alecr95@gmail.com>
2017-12-11 02:17:27 -0500

    add a spy

--- src/test/java/TrainModel/TrainModelTest.java
+++ src/test/java/TrainModel/TrainModelTest.java
@@ -162,13 +162,18 @@ public class TrainModelTest{
     @Test
     public void testUpdateSpeed(){
         TrackModel _tm = mock(TrackModel.class);
-        when(_tm.getGrade(testTrainObject.getTrainId())).thenReturn(0.5);
+
         testTrainObject = new Train(1, 0, _tm, 1);
         testTrainObject.velocity = 10.0;
         testTrainObject.numPassengers = 444;
-        testTrainObject.updateSpeed(10000);
-        //testTrainObject.updateSpeed(10000) = 10.2454971747 (hand calculation)
-        assertEquals(10.2454971747, testTrainObject.getCurrentVelocity(), delta);
+
+        Train spyTrain = spy(testTrainObject);
+        doReturn(0.5).when(spyTrain).getGrade();
+
+        
+        spyTrain.updateSpeed(10000);
+        //spyTrain.updateSpeed(10000) = 10.2454971747 (hand calculation)
+        assertEquals(10.2454971747, spyTrain.getCurrentVelocity(), delta);
 
     }
 
